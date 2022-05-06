f96e175ed9ff597fc5c807868091a5362b5729c0
Rakhchik <ilyachannle@gmail.com>
2022-05-06 20:40:04 +0300

    Removed AuthorizationTokenIsNullException

--- src/main/java/ru/rakhcheev/tasket/api/tasketapi/exception/AuthorizationTokenIsNullException.java
+++
@@ -1,7 +0,0 @@
-package ru.rakhcheev.tasket.api.tasketapi.exception;
-
-public class AuthorizationTokenIsNullException extends Exception {
-    public AuthorizationTokenIsNullException(String message) {
-        super(message);
-    }
-}
