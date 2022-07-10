d0e2a6acf0ae4e2ebce63a5cc39a27b6837e14f4
Rakhcha <i.s.rakhcheev@gmail.com>
2022-07-10 16:30:19 +0300

    Rename fields and add Data annotation

--- src/main/java/ru/rakhcheev/tasket/api/tasketapi/entity/DescriptionEntity.java
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/entity/DescriptionEntity.java
@@ -1,21 +1,32 @@
 package ru.rakhcheev.tasket.api.tasketapi.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "description")
+@Data
 public class DescriptionEntity {
 
     @Id
-    private Long user_id;
+    @Column(name = "user_id")
+    private Long userId;
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "surname")
     private String surname;
-    private String patronymic;
+
+    @Column(name = "city")
     private String city;
-    private String phone_number;
+
+    @Column(name = "number")
+    private String  Number;
+
+    @Column(name = "about")
     private String about;
 
     @OneToOne(fetch = FetchType.LAZY)
@@ -23,67 +34,4 @@ public class DescriptionEntity {
     @JsonIgnore
     private UserEntity user;
 
-    public Long getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getPatronymic() {
-        return patronymic;
-    }
-
-    public void setPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getPhone_number() {
-        return phone_number;
-    }
-
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
-    public String getAbout() {
-        return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
-    }
-
-    public UserEntity getUser() {
-        return user;
-    }
-
-    public void setUser(UserEntity user) {
-        this.user = user;
-    }
 }
--- src/main/java/ru/rakhcheev/tasket/api/tasketapi/entity/UserEntity.java
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/entity/UserEntity.java
@@ -1,12 +1,14 @@
 package ru.rakhcheev.tasket.api.tasketapi.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
 
 import javax.persistence.*;
 
 
 @Entity
 @Table(name = "user")
+@Data
 public class UserEntity {
 
     @Id
@@ -30,42 +32,6 @@ public class UserEntity {
     private DescriptionEntity description;
 
     public UserEntity() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public DescriptionEntity getDescription() {
-        return description;
     }
 
     public void setDescription(DescriptionEntity description) {
