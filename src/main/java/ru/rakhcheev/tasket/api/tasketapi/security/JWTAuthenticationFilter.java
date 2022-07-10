d5a110a36b5c84858a0d33f4e01c635e29d2fed9
Rakhcha <i.s.rakhcheev@gmail.com>
2022-07-10 16:39:09 +0300

    Update login endpoint

--- src/main/java/ru/rakhcheev/tasket/api/tasketapi/security/JWTAuthenticationFilter.java
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/security/JWTAuthenticationFilter.java
@@ -3,19 +3,26 @@ package ru.rakhcheev.tasket.api.tasketapi.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.HttpClientErrorException;
 import ru.rakhcheev.tasket.api.tasketapi.entity.UserEntity;
+import ru.rakhcheev.tasket.api.tasketapi.exception.UserDatabaseIsEmptyException;
+import ru.rakhcheev.tasket.api.tasketapi.exception.UserNotFoundException;
+import ru.rakhcheev.tasket.api.tasketapi.services.UserService;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -23,12 +30,13 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     private final AuthenticationManager authenticationManager;
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.setFilterProcessesUrl("/users/login");
+        this.setFilterProcessesUrl("/login");
         this.authenticationManager = authenticationManager;
     }
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
 
         try {
             UserEntity userData = new ObjectMapper().readValue(request.getInputStream(), UserEntity.class);
