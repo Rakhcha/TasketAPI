78c28a29ca217dc3a78f0517d629b5b8fb13c814
Rakhcha <i.s.rakhcheev@gmail.com>
2022-07-15 15:19:21 +0300

    Added endpoints for request to get All communities and get Community by ID

--- src/main/java/ru/rakhcheev/tasket/api/tasketapi/controller/CommunityController.java
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/controller/CommunityController.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.rakhcheev.tasket.api.tasketapi.dto.community.CommunityCreationDTO;
-import ru.rakhcheev.tasket.api.tasketapi.exception.CommunityAlreadyExistException;
-import ru.rakhcheev.tasket.api.tasketapi.exception.UserHasNotPermissionToCreateMoreThanOneCommunityException;
+import ru.rakhcheev.tasket.api.tasketapi.dto.community.CommunityDTO;
+import ru.rakhcheev.tasket.api.tasketapi.dto.community.CommunityInfoDTO;
+import ru.rakhcheev.tasket.api.tasketapi.exception.*;
 import ru.rakhcheev.tasket.api.tasketapi.services.CommunityService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/community")
@@ -31,7 +31,7 @@ public class CommunityController {
         try {
             communityService.addCommunity(userLogin, community);
             return new ResponseEntity<>("Группа" + community.getCommunityName() + " добавлена.", HttpStatus.OK);
-        } catch (UserHasNotPermissionToCreateMoreThanOneCommunityException e) {
+        } catch (UserHasNotPermission e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
         } catch (CommunityAlreadyExistException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
@@ -40,9 +40,37 @@ public class CommunityController {
         }
     }
 
-    // Get all communities
+    @GetMapping
+    public ResponseEntity<?> getCommunities(@RequestParam(value = "type", defaultValue = "public") String type,
+                                                     Authentication authentication) {
+        try {
+            List<CommunityInfoDTO> communityList = communityService.getCommunities(type, authentication.getName());
+            return new ResponseEntity<>(communityList, HttpStatus.OK) ;
+        } catch (CommunityEnumTypeIsNotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (TableIsEmptyException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e){
+            return new ResponseEntity<>("Произошла непредвиденная ошибка: " + e.getCause(), HttpStatus.BAD_REQUEST);
+        }
 
-    // Get community by ID
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> getCommunityById(@PathVariable(value = "id") Long id,
+                                            Authentication authentication) {
+        try {
+            CommunityDTO communityList = communityService.getCommunityById(id, authentication.getName());
+            return new ResponseEntity<>(communityList, HttpStatus.OK) ;
+        } catch (CommunityNotFoundException | UserHasNotPermission e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e){
+            return new ResponseEntity<>("Произошла непредвиденная ошибка: " + e.getCause(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Get community by Name
 
     // Delete community by ID
 
