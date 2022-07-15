1c6dc8db0672e95f64da4efc347b80194ef00b43
Rakhcha <i.s.rakhcheev@gmail.com>
2022-07-15 15:18:18 +0300

    Added new Exceptions

---
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/exception/CommunityEnumTypeIsNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.rakhcheev.tasket.api.tasketapi.exception;
+
+public class CommunityEnumTypeIsNotFoundException extends Exception{
+
+    public CommunityEnumTypeIsNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/exception/CommunityNotFoundException.java
@@ -0,0 +1,11 @@
+package ru.rakhcheev.tasket.api.tasketapi.exception;
+
+public class CommunityNotFoundException extends Exception{
+
+    public CommunityNotFoundException() {
+    }
+
+    public CommunityNotFoundException(String message) {
+        super(message);
+    }
+}
