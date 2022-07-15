2ad80717bcc78eefabd6f086bbd59ea99f0addf9
Rakhcha <i.s.rakhcheev@gmail.com>
2022-07-15 15:15:59 +0300

    renamed from UserHasNotPermissionToCreateMoreThanOneCommunityException

---
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/exception/UserHasNotPermission.java
@@ -0,0 +1,8 @@
+package ru.rakhcheev.tasket.api.tasketapi.exception;
+
+public class UserHasNotPermission extends Exception{
+
+    public UserHasNotPermission(String message) {
+        super(message);
+    }
+}
--- src/main/java/ru/rakhcheev/tasket/api/tasketapi/exception/UserHasNotPermissionToCreateMoreThanOneCommunityException.java
+++
@@ -1,8 +0,0 @@
-package ru.rakhcheev.tasket.api.tasketapi.exception;
-
-public class UserHasNotPermissionToCreateMoreThanOneCommunityException extends Exception{
-
-    public UserHasNotPermissionToCreateMoreThanOneCommunityException() {
-        super("Нет прав для создания более одной группы");
-    }
-}
