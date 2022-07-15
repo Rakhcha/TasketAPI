2db97b73cc46abcc40329f4c8e73d9a98771b1ce
Rakhcha <i.s.rakhcheev@gmail.com>
2022-07-15 15:19:39 +0300

    Added two DTO for Community

---
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/dto/community/CommunityDTO.java
@@ -0,0 +1,37 @@
+package ru.rakhcheev.tasket.api.tasketapi.dto.community;
+
+import lombok.Data;
+import ru.rakhcheev.tasket.api.tasketapi.dto.user.UserDTO;
+import ru.rakhcheev.tasket.api.tasketapi.entity.CommunityEntity;
+import ru.rakhcheev.tasket.api.tasketapi.entity.UserEntity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class CommunityDTO {
+
+    private String communityName;
+    private Boolean isPrivate;
+    private UserDTO creator;
+    private Set<UserDTO> users;
+
+    public static CommunityDTO toDTO(CommunityEntity communityEntity){
+        CommunityDTO communityDTO = new CommunityDTO();
+        communityDTO.setCommunityName(communityEntity.getCommunityName());
+        communityDTO.setIsPrivate(communityEntity.getIsPrivate());
+
+        UserEntity user = communityEntity.getCreator();
+        user.setDescription(null);
+        communityDTO.setCreator(UserDTO.toDTO(user));
+
+        Set<UserDTO> users = new HashSet<>();
+        for(UserEntity userEntity : communityEntity.getUsersSet()){
+            userEntity.setDescription(null);
+            users.add(UserDTO.toDTO(userEntity));
+        }
+        communityDTO.setUsers(users);
+        return communityDTO;
+    }
+
+}
---
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/dto/community/CommunityInfoDTO.java
@@ -0,0 +1,21 @@
+package ru.rakhcheev.tasket.api.tasketapi.dto.community;
+
+import lombok.Data;
+import ru.rakhcheev.tasket.api.tasketapi.entity.CommunityEntity;
+
+@Data
+public class CommunityInfoDTO {
+
+    private String communityName;
+    private String creatorLogin;
+    private int countOfUsers;
+
+    public static CommunityInfoDTO toDTO(CommunityEntity entity){
+        CommunityInfoDTO communityDTO = new CommunityInfoDTO();
+        communityDTO.setCommunityName(entity.getCommunityName());
+        communityDTO.setCreatorLogin(entity.getCreator().getLogin());
+        communityDTO.setCountOfUsers(entity.getUsersSet().size());
+        return communityDTO;
+    }
+
+}
