a338cd88bfaa02303631d9029364c205cad0c226
Rakhcha <i.s.rakhcheev@gmail.com>
2022-07-15 18:24:03 +0300

    Added id in DTO

--- src/main/java/ru/rakhcheev/tasket/api/tasketapi/dto/community/CommunityInfoDTO.java
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/dto/community/CommunityInfoDTO.java
@@ -6,12 +6,14 @@ import ru.rakhcheev.tasket.api.tasketapi.entity.CommunityEntity;
 @Data
 public class CommunityInfoDTO {
 
+    private Long communityID;
     private String communityName;
     private String creatorLogin;
     private int countOfUsers;
 
-    public static CommunityInfoDTO toDTO(CommunityEntity entity){
+    public static CommunityInfoDTO toDTO(CommunityEntity entity) {
         CommunityInfoDTO communityDTO = new CommunityInfoDTO();
+        communityDTO.setCommunityID(entity.getCommunityId());
         communityDTO.setCommunityName(entity.getCommunityName());
         communityDTO.setCreatorLogin(entity.getCreator().getLogin());
         communityDTO.setCountOfUsers(entity.getUsersSet().size());
