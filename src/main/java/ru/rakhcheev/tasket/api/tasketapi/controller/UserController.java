c7fbf81577d3daabe2bd84a2cbef272aab91bb1b
Rakhcha <i.s.rakhcheev@gmail.com>
2022-07-20 22:00:33 +0300

    Added exception and update request body dto

--- src/main/java/ru/rakhcheev/tasket/api/tasketapi/controller/UserController.java
+++ src/main/java/ru/rakhcheev/tasket/api/tasketapi/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.rakhcheev.tasket.api.tasketapi.dto.user.UserCreationDTO;
 import ru.rakhcheev.tasket.api.tasketapi.dto.user.UserDTO;
-import ru.rakhcheev.tasket.api.tasketapi.entity.UserEntity;
 import ru.rakhcheev.tasket.api.tasketapi.exception.UserAlreadyExistException;
 import ru.rakhcheev.tasket.api.tasketapi.exception.UserDatabaseIsEmptyException;
 import ru.rakhcheev.tasket.api.tasketapi.exception.UserNotFoundException;
@@ -60,16 +59,20 @@ public class UserController {
 
     @PutMapping(value = "/{id}")
     public ResponseEntity<String> editUser(@PathVariable Long id,
-                                           @RequestBody UserEntity newUserParams) {
+                                           @RequestBody UserCreationDTO newUserParams) {
         try {
             String login = userService.editUser(id, newUserParams);
             return new ResponseEntity<>("Данные пользователя " + login + " изменены.", HttpStatus.OK);
         } catch (UserDatabaseIsEmptyException | UserNotFoundException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>("Попытка ввода несуществующей роли", HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             return new ResponseEntity<>("Произошла непредвиденная ошибка: " + e.getCause(), HttpStatus.BAD_REQUEST);
         }
     }
+
+    // TODO add edit user by Authentication
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
