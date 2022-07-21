9e0a7115c432d5b02ceab9b5312664f295f26130
Rakhcha <i.s.rakhcheev@gmail.com>
2022-07-21 12:38:55 +0300

    Delete not usage Exception

--- src/main/java/ru/rakhcheev/tasket/api/tasketapi/exception/DescriptionIsNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.rakhcheev.tasket.api.tasketapi.exception;
-
-public class DescriptionIsNotFoundException extends Exception {
-    public DescriptionIsNotFoundException(String message) {
-        super(message);
-    }
-}
