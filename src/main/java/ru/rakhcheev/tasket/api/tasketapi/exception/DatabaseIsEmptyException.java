04f9d02c98dd3514eb97f6042e1bd78b953af18e
Rakhcha <i.s.rakhcheev@gmail.com>
2022-07-26 17:38:32 +0300

    Delete DatabaseIsEmptyException.java

--- src/main/java/ru/rakhcheev/tasket/api/tasketapi/exception/DatabaseIsEmptyException.java
+++
@@ -1,7 +0,0 @@
-package ru.rakhcheev.tasket.api.tasketapi.exception;
-
-public class DatabaseIsEmptyException extends Exception {
-    public DatabaseIsEmptyException(String message) {
-        super(message);
-    }
-}
